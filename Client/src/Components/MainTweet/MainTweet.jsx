c1150ea5baa9692cf60bdfa001c45a443fedb02a
ShankarSharma1601 <shankarsharma1601@gmail.com>
2023-05-31 19:40:32 +0530

    Left & Right Sidebar UI

---
+++ Client/src/Components/MainTweet/MainTweet.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MainTweet = () => {
+  return (
+    <div>
+      <p className="font-bold pl-2 my-2">UserName</p>
+      <form className="border-b-2 pb-6">
+        <textarea className="bg-slate-200 rounded-lg 2-full p-2"></textarea>
+      </form>
+    </div>
+  );
+};
+
+export default MainTweet;
